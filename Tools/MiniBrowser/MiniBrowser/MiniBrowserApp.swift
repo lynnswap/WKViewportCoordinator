095116caff92bda11ad1e01d8b725efe94dd9580
Kazuki Nakashima <65545348+lynnswap@users.noreply.github.com>
2026-04-03 17:23:12 +0900

    init

---
+++ Tools/MiniBrowser/MiniBrowser/MiniBrowserApp.swift
@@ -0,0 +1,17 @@
+//
+//  MiniBrowserApp.swift
+//  MiniBrowser
+//
+//  Created by Kazuki Nakashima on 2026/04/03.
+//
+
+import SwiftUI
+
+@main
+struct MiniBrowserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
